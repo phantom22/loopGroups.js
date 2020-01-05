57e356ab172b73c081594072ea45feb093d29b3c
Maksym Naumenko <maksymnaumenko2002@mail.ru>
2020-01-06 00:30:39 +0100

    Add files via upload

---
+++ animationGroups.js
@@ -0,0 +1,229 @@
+/**
+ * container for animated groups of elements
+ * @constructor
+ * @param {Array}            root            array of HTMLElement query 
+ */
+class Controller {
+
+	constructor( ...root ) {
+
+		if ( Array.isArray(root) && root.length >= 1 ) {
+
+			root.forEach( root => {
+
+				const e = document.querySelectorAll(root);
+
+				if ( e.length >= 1 ) {
+
+					let elements = {};
+
+					for ( let i = 0; i < e.length; i++ ) {
+
+						const styles = getComputedStyle(e[i]);
+
+						elements[i] = { element: e[i], style: { width: Number(styles.width.replace("px","")) }, values: { verse: 0, onpause: false } };
+
+					}
+
+					this.elements = elements;
+
+				}
+
+			})
+
+		}
+
+	}
+
+}
+
+class stretchAnimation extends Controller {
+
+	constructor( ...root ) {
+
+		if ( Array.isArray(root) && root.length >= 1 ) {
+
+			super( root );
+
+		}
+
+	}
+
+	setProperties( properties = { sWidth, fWidth, stretchSpeed, initialDelay, rest } ) {
+
+		const eLength = Object.keys(this.elements).length,
+		validate = (arr) => Array.isArray(arr) && arr.length === eLength,
+		{ sWidth, fWidth, stretchSpeed, initialDelay, rest } = properties;
+
+		if ( validate(sWidth) && validate(fWidth) && validate(stretchSpeed) && validate(initialDelay) && validate(rest) ) {
+
+			const p = Object.keys(properties);
+
+			for ( let i = 0; i < p.length; i++ ) {
+
+				const property = p[i];
+
+				for ( let I = 0; I < properties[property].length; I++ ) {
+
+					if ( property === "sWidth" ) { this.elements[I].values.starting = { width: properties[property][I] }; this.elements[I].style.width = properties[property][I] }
+					else if ( property === "fWidth" ) { this.elements[I].values.final = { width: properties[property][I] } }
+					else if ( property === "initialDelay" ) { this.elements[I].values[property] = properties[property][I]; this.elements[I].values.onpause = properties[property][I] > 0 ? true : false; }
+					else { this.elements[I].values[property] = properties[property][I] }
+
+				}
+
+			}
+
+		}
+
+	}
+
+	HTMLUpdate() {
+
+		const eLength = Object.keys(this.elements).length;
+		
+		for ( let i = 0; i < eLength; i++ ) {
+
+			this.elements[i].element.style.width = `${this.elements[i].style.width}%`;
+			this.elements[i].element.style.height = `${this.elements[i].style.height}px`;
+
+		}
+
+	}
+
+	calculateObjectsWidth() {
+
+		if ( typeof this.animation !== "undefined" ) {
+
+			for ( let i = 0; i < Object.keys(this.elements).length; i++ ) {
+
+				if ( this.elements[i].values.onpause === false ) {
+
+					let verse = this.elements[i].values.verse,
+					width = this.elements[i].style.width;
+
+					const speed = verse === 0 ? this.elements[i].values.stretchSpeed : -this.elements[i].values.stretchSpeed,
+					fWidth = this.elements[i].values.final.width,
+					sWidth = this.elements[i].values.starting.width,
+					fps = this.animation.fps;
+
+					width += (speed / fps);
+
+					width = width < sWidth ? sWidth : width;
+					width = width > fWidth ? fWidth : width;
+
+					this.elements[i].style.width = width;
+
+					if ( verse === 0 && width >= fWidth ) {
+
+						this.elements[i].values.verse = 1;
+						this.rest(i);
+
+					}
+
+					else if ( verse === 1 && width <= sWidth ) {
+
+						this.elements[i].values.verse = 0;
+						this.rest(i);
+
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+
+	initialDelays() {
+
+		if ( typeof this.animation !== "undefined" ) {
+
+			const eLength = Object.keys(this.elements).length;
+
+			for ( let i = 0; i < eLength; i++ ) {
+
+				if ( this.elements[i].values.onpause === true && this.elements[i].values.initialDelay > 0 ) {
+
+					setTimeout(function(A,B){
+
+						A.elements[B].values.onpause = false;
+
+					}, this.elements[i].values.initialDelay, this, i)
+
+				}
+
+			}
+
+			this.animation.initialDelays = true;
+
+		}
+
+	}
+
+	startLoop( fps ) {
+
+		if ( typeof fps === "number" && fps >= 1 ) {
+
+			this.animation = { onpause: false, fps: fps, initialDelays: false };
+
+			this.animation.display = setInterval(function(A){
+
+				if ( A.animation.initialDelays === false ) {
+
+					A.initialDelays();
+				}
+
+				if ( A.animation.onpause === false ) {
+
+					A.HTMLUpdate();
+					A.calculateObjectsWidth();
+
+				}
+
+			}, (1000 / fps), this)
+			
+		}
+
+	}
+
+	pause() {
+
+		if ( typeof this.animation !== "undefined" ) {
+
+			this.animation.onpause = this.animation.onpause === false ? true : false;
+
+		}
+
+	}
+
+	reset() {
+
+		if ( typeof this.animation !== "undefined" ) {
+
+			clearInterval( this.animation.display );
+			delete this.animation;
+
+		}
+
+	}
+
+	rest( element ) {
+
+		if ( typeof this.elements[element] !== "undefined" ) {
+
+			this.elements[element].values.onpause = true;
+
+			setTimeout(function(A,B){
+
+				A.elements[B].values.onpause = false;
+
+			}, this.elements[element].values.rest, this, element)
+
+
+		}
+
+	}
+
+}
--- main.js
+++ main.js
@@ -1,53 +1,11 @@
-let t1 = new animation(D.gE("element1"));
-let t2 = new animation(D.gE("element2"));
-let t3 = new animation(D.gE("element3"));
-let t4 = new animation(D.gE("element4"));
-let t5 = new animation(D.gE("element5"));
-let t6 = new animation(D.gE("element6"));
-let t7 = new animation(D.gE("element7"));
-let t8 = new animation(D.gE("element8"));
-let t9 = new animation(D.gE("element9"));
-let t10 = new animation(D.gE("element10"));
-let t11 = new animation(D.gE("element11"));
-let t12 = new animation(D.gE("element12"));
-let t13 = new animation(D.gE("element13"));
-let t14 = new animation(D.gE("element14"));
-let t15 = new animation(D.gE("element15"));
-let t16 = new animation(D.gE("element16"));
-let t17 = new animation(D.gE("element17"));
-let t18 = new animation(D.gE("element18"));
-let t19 = new animation(D.gE("element19"));
-let t20 = new animation(D.gE("element20"));
+let bg = new stretchAnimation([".macros1"]);
 
-let m1 = new multiAnimation(t1);
-let m2 = new multiAnimation(t2);
-let m3 = new multiAnimation(t3);
-let m4 = new multiAnimation(t4);
-let m5 = new multiAnimation(t5);
-let m6 = new multiAnimation(t6);
-let m7 = new multiAnimation(t7);
-let m8 = new multiAnimation(t8);
-let m9 = new multiAnimation(t9);
-let m10 = new multiAnimation(t10);
-let m11 = new multiAnimation(t11);
-let m12 = new multiAnimation(t12);
-let m13 = new multiAnimation(t13);
-let m14 = new multiAnimation(t14);
-let m15 = new multiAnimation(t15);
-let m16 = new multiAnimation(t16);
-let m17 = new multiAnimation(t17);
-let m18 = new multiAnimation(t18);
-let m19 = new multiAnimation(t19);
-let m20 = new multiAnimation(t20);
+bg.setProperties({
+	sWidth:          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	fWidth:          [10,20,30,40,50,60,70,80,90,100,100,90,80,70,60,50,40,30,20,10],
+	stretchSpeed:    [1.8,3.6,5.4,7.2,9,10.8,12.6,14.4,16.2,18,18,16.2,14.4,12.6,10.8,9,7.2,5.4,3.6,1.8],
+	initialDelay:    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	rest:            [19000,17000,15000,13000,11000,9000,7000,5000,3000,1000,0,2000,4000,6000,8000,10000,12000,14000,16000,18000]
+});
 
-let manager = new animationManager();
-
-(function(){
-
-let macrosArr = [m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13,m14,m15,m16,m17,m18,m19,m20];
-let animationArr = [[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5],[0,100,5]];
-let delres = [[0,500],[500,500],[1000,500],[1500,500],[2000,500],[2500,500],[3000,500],[3500,500],[4000,500],[4500,500],[5000,500],[5500,500],[6000,500],[6500,500],[7000,500],[7500,500],[8000,500],[8500,500],[9000,500],[9500,500]];
-
-manager.loopDelay(macrosArr,"stretch",animationArr,"x",delres);
-
-})()
+bg.startLoop(62.51);
